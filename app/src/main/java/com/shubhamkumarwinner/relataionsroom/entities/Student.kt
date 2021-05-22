91c638c6b3a351cf3acfc6554996d73545f2641d
Shubham <shubhamkumarwinner@gmail.com>
2021-05-22 19:05:31 +0530

    One to Many relation in room setup

--- app/src/main/java/com/shubhamkumarwinner/relataionsroom/entities/Student.kt
+++ app/src/main/java/com/shubhamkumarwinner/relataionsroom/entities/Student.kt
@@ -1,7 +1,9 @@
 package com.shubhamkumarwinner.relataionsroom.entities
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class Student(
     @PrimaryKey(autoGenerate = false)
     val studentName: String,
